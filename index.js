3382e8999a79604a62c970d70229a937e76b5ec7
Muhammad Irsyad <irsyad0906@gmail.com>
2025-10-17 22:04:04 +0700

    fix: update fallback route to correctly serve index.html for all paths

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./src/routes');
+// const routes = require('./src/routes'); // <-- Sementara nonaktifkan baris ini
 require('dotenv').config();
 
 const app = express();
 
-// Middleware untuk parsing JSON body
 app.use(express.json());
-
-// Middleware untuk menyajikan file statis dari folder 'public'
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use('/api', routes); // <-- Sementara nonaktifkan baris ini
 
-// Gunakan semua route yang telah didefinisikan
-app.use('/api', routes); // Menambahkan prefix /api untuk semua rute
-
-// Fallback untuk mengirim index.html jika tidak ada route API yang cocok
-app.get('/*', (req, res) => {
+app.get('/*any', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
